6ad1aa383604f7a0df041ac4f3c11ac32ce981a6
aksaccount <35652014+aksaccount@users.noreply.github.com>
2018-01-21 14:09:54 +0530

    Update positive.c

--- .gitignore/positive.c
+++ .gitignore/positive.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-
-int main(void) {
+#include<stdio.h>
+void main() {
 	int n;
 	scanf("%d",&n);
 	if(n>0)
 	printf("positive");
 	else if(n<0)
 	printf("negative");
-	else
+	else if(n==0)
 	printf("zero");
-	return 0;
+	else
+	printf("invalid input");
 }
